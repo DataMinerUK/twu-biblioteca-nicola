cf8bf2d94851c30aece98b23015d954a340a9dcb
Nicola Hughes <nihughes@eunihughes.local>
2016-03-02 15:51:50 +0000

    Adding a welcome message to main

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        WelcomeMessage welcome = new WelcomeMessage(System.out);
+        welcome.printMessage();
     }
 }
