f007483b1df4fde775c4147ee990f7b83a3ce143
Nicola Hughes <nihughes@eunihughes.local>
2016-03-04 13:10:43 +0000

    Creating library item superclass which is inherited by book

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -5,42 +5,25 @@ import java.io.PrintStream;
 /**
  * Created by nihughes on 02/03/2016.
  */
-public class Book {
-    private String title;
+public class Book extends LibraryItem{
+
     private String author;
-    private int year;
-    private boolean checkedOut;
 
     public Book(String t, String a, int y){
+        super();
         this.title = t;
         this.author = a;
         this.year = y;
         this.checkedOut = false;
     }
 
-    public String getTitle(){
-        return title;
-    }
-
     public String getAuthor(){
         return author;
     }
 
-    public int getYear() {
-        return year;
-    }
-
-    public boolean isCheckedOut(){ return checkedOut; }
-
+    @Override
     public void printDetails(PrintStream printStream){
         printStream.println(getTitle()+"\t"+getAuthor()+"\t"+getYear());
     }
-
-    public void checkOut(){
-        checkedOut = true;
-    }
-
-    public void checkIn(){
-        checkedOut = false;
-    }
+    
 }
--- src/com/twu/biblioteca/LibraryItem.java
+++ src/com/twu/biblioteca/LibraryItem.java
@@ -1,7 +1,36 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 /**
  * Created by nihughes on 04/03/2016.
  */
-public class LibraryItem {
+public abstract class LibraryItem {
+
+    public String title;
+    protected int year;
+    protected boolean checkedOut;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isCheckedOut(){ return checkedOut; }
+
+    public void checkOut(){
+        checkedOut = true;
+    }
+
+    public void checkIn(){
+        checkedOut = false;
+    }
+
+    public abstract void printDetails(PrintStream printStream);
+
+
+
 }
